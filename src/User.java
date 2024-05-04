e3726f8bf3b53f55ec65ac8f3c6ac9d1f287ed45
cesariin <cesarin_navas@hotmail.com>
2024-05-04 12:20:54 -0600

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,8 @@
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+    // Getters and setters
+}
